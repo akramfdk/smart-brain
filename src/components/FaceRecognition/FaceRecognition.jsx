c8878e74b35ee6cec3e80abc028ad62982abddc2
akramfdk <akramjeet.singh@gmail.com>
2025-09-18 23:28:05 +1000

    Added components for signin and register, basic functionality using state variable route to signin and signout

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,11 +1,11 @@
 import './FaceRecognition.css'
 
 const FaceRecognition = ({ imageUrl, box }) => {
-    console.log(box);
+    // console.log(box);
     return (
         <div className="center ma">
-            <div className="absolute mt2">
-                <img id="inputimage" src={imageUrl} alt="" width="500px" height="auto" />
+            <div className="relative mt2">
+                {imageUrl && <img id="inputimage" src={imageUrl} alt="" width="500px" height="auto" />}
                 {box && <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>}
             </div>
         </div>
