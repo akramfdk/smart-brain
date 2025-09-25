672e4599f23eadd21097d81eef5a2839b0e69eda
akramfdk <akramjeet.singh@gmail.com>
2025-09-25 23:12:25 +1000

    fixed bug that the intial state is reset when a user logs out

--- src/App.jsx
+++ src/App.jsx
@@ -49,25 +49,26 @@ const getURLrequestOptions = (imageUrl) => {
   return requestOptions;
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
 
   constructor(){
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -133,6 +134,7 @@ class App extends Component {
                 console.log(count);
                 this.setState(Object.assign(this.state.user, {entries: count}));
               })
+              .catch(console.log)
           }
           this.displayFaceBox(this.calculateFaceLocation(result))
         })
@@ -141,7 +143,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout'){
-      this.setState({isSignedIn: false});
+      this.setState(initialState);
     } else if (route === 'home'){
       this.setState({isSignedIn: true});
     }
